1bee89bcfd12fb6da402f5fee9b8e47402cdb6d3
christhopermarcelino <christhopermarcelino@gmail.com>
2024-08-29 09:37:43 +0700

    feat: add root route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,13 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function() {
+    return response()->json([
+        'success' => true,
+        'message' => 'success'
+    ], 200);
+});
+
 Route::middleware(['auth-jwt', 'api'], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/me', function() {
